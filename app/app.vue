e93bf477eb0c803ef7933050bf7d30f418fd808e
motttey <motitago@gmail.com>
2025-12-17 00:01:49 +0900

    fix: landing content

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,67 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <!--
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    -->
+  <div
+    class="fixed w-full flex justify-end bottom-0 pb-3 pr-5 transition"
+  >
+    <div 
+      class="flex flex-row justify-center items-center"
+      id="abstract"
+    >
+      <div class="grid grid-cols-1 sm:grid-cols-2">
+        <div class="h-72 sm:h-screen items-center text-center">
+          <img 
+            class="cover-img w-full h-full sm:h-screen object-center object-cover"
+            loading="lazy"
+            src="https://pbs.twimg.com/media/G8GwjjrbQAAGUUo?format=jpg" 
+            alt="FS表紙画像"
+          />
+        </div>
+        <div class="p-6 mx-8 my-6 items-center align-middle bg-contain bg-no-repeat max-w-none">
+          <h2 class="text-lg md:text-2xl font-bold">藤子不二雄オマージュマガジン</h2>
+          <h1 class="text-2xl md:text-3xl font-bold">フジコ・スピリット</h1>
+          <p class="text-right text-md md:text-lg font-semibold max-w-lg">by 
+            <a class="text-blue-600" href="https://webcatalog-free.circle.ms/Circle/16802791">カンヅメカン</a>
+          </p>
+          <blockquote class="text-md text-gray-900 dark:text-white my-10">
+            <p class="text-justify text-sm font-semibold max-w-lg">
+              日本を代表するトップクリエイターの多くは、インタビュー等を通して「自分の創作活動の原体験は藤子不二雄作品にある」と語っています。<br>
+              「フジコ・スピリット 第0号」では、SNSで活動される藤子作品を愛してやまない11名の創作家による漫画・イラスト・小説をオムニバス形式で掲載させていただきました。
+            </p>
+            <p class="text-right text-md font-bold max-w-lg">
+              ― 主催 ともあき・ザ・ビッグシティ (<a class="text-blue-600" href="https://twitter.com/yjmtomoaki" target="_blank">@yjmtomoaki</a>)
+            </p>
+          </blockquote>
+          <img 
+            class="my-4 items-center text-center max-w-sm w-full object-full"
+            src="https://pbs.twimg.com/media/G8GwjjrbQAAGUUo?format=jpg" 
+            alt="FSのと表紙/裏表紙"
+          />
+          <h2 class="my-4 text-xl md:text-2xl font-medium">
+            詳細情報
+          </h2>
+          <p class="my-4 text-l md:text-xl">第3号をC107で頒布します</p>
+          <ul class="space-y-1 max-w-md text-sm list-disc list-inside">
+            <li>
+              コミックマーケット107 (C107)
+            </li>
+            <li>
+              頒布場所: <span class="font-semibold">1日目(12/30) 東4イ03-a パラレルソレイユ</span>
+            </li>
+            <li>
+              頒布価格: <span class="font-semibold">2,000円</span>
+            </li>
+            <li>
+              内容: <span class="font-semibold">A5 288ページ</span>
+            </li>
+            <!--
+            <li>
+              通販: <span class="font-semibold">
+                <a class="text-blue-600" href="https://yjmtomoaki.booth.pm/items/4446490">[BOOTH]</a>
+              </span>
+            </li> 
+            -->
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
