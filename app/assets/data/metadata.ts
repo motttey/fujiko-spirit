79b3d26a28ad3e2027cd406b37b6da5ccf91abc5
motttey <motitago@gmail.com>
2025-12-21 22:32:16 +0900

    add: author list

--- app/assets/data/metadata.ts
+++ app/assets/data/metadata.ts
@@ -42,3 +42,196 @@ export const FS_BACK_NUMBER = [
     onlineOrderUrl: "https://yjmtomoaki.booth.pm/items/5330491",
   },
 ];
+
+export const FS3_AUTHOR_LIST = [
+  {
+    author: "ありソラ",
+    title: "ジツゲンの芽",
+    type: "漫画",
+  },
+  {
+    author: "いびりょ",
+    title: "気がついたら弱者男性代表の顔になった件",
+    type: "漫画",
+  },
+  {
+    author: "今川宇宙",
+    title: "戯曲 「待機室のこと」",
+    type: "脚本",
+  },
+  {
+    author: "うろこ",
+    title: "魔法少女とよんでくれ",
+    type: "漫画",
+  },
+  {
+    author: "かば島・C・でか子",
+    title: "深窓探偵 工藤旬架の謎解き手帖",
+    type: "漫画",
+  },
+  {
+    author: "きのしき",
+    title: "働く動物たち",
+    type: "漫画",
+  },
+  {
+    author: "巨大迷路",
+    title: "たんすの脅威から逃れよう！",
+    type: "漫画",
+  },
+  {
+    author: "黒幕組合",
+    title: "般若、真蛇ぅ",
+    type: "漫画",
+  },
+  {
+    author: "Ｋ.M",
+    title: "師に至る彷徨い",
+    type: "漫画",
+  },
+  {
+    author: "心もとない天使",
+    title: "マルチデリッダー",
+    type: "漫画",
+  },
+  {
+    author: "高台県一",
+    title: "アイデアのたまご",
+    type: "漫画",
+  },
+  {
+    author: "坂崎晶也",
+    title: "助手席の神様",
+    type: "漫画",
+  },
+  {
+    author: "塩ぱん",
+    title: "６番、アレコレサワルナー",
+    type: "小説",
+  },
+  {
+    author: "シャンパー",
+    title: "ザリニガ",
+    type: "漫画",
+  },
+  {
+    author: "スーさん",
+    title: "電脳人妻魔法少女チサト 殻をやぶれ編～",
+    type: "漫画",
+  },
+  {
+    author: "ぞの",
+    title: "プロポーズ大作戦",
+    type: "漫画",
+  },
+  {
+    author: "千葉太郎",
+    title: "サルのモラトリアム",
+    type: "漫画",
+  },
+  {
+    author: "ツイートおじさん",
+    title: "殻落",
+    type: "漫画",
+  },
+  {
+    author: "ドラ好き漫画家志望",
+    title: "メンタルが弱すぎるヒヨコ",
+    type: "漫画",
+  },
+  {
+    author: "にわっち",
+    title: "爆誕卵(ばくたんたまご)",
+    type: "漫画",
+  },
+  {
+    author: "ねこしか",
+    title: "星の剪定者",
+    type: "漫画",
+  },
+  {
+    author: "ひぐちあき",
+    title: "マンガニア2026",
+    type: "漫画",
+  },
+  {
+    author: "ひなとっと",
+    title: "日常ほのぼの系漫画には何故男がいないのか",
+    type: "漫画",
+  },
+  {
+    author: "北條蒼大",
+    title: "アイデアのたまご",
+    type: "漫画",
+  },
+  {
+    author: "ボスペクター",
+    title: "Mishmash",
+    type: "イラスト",
+  },
+  {
+    author: "ぽちじるし",
+    title: "そう遠くない未来の話",
+    type: "漫画",
+  },
+  {
+    author: "枕さん",
+    title: "からっぽ",
+    type: "漫画",
+  },
+  {
+    author: "マサえもん",
+    title: "看板娘さん。",
+    type: "漫画",
+  },
+  {
+    author: "まなし",
+    title: "無題",
+    type: "漫画",
+  },
+  {
+    author: "ミオリ",
+    title: "サンパチ・ドラキュリータ",
+    type: "漫画",
+  },
+  {
+    author: "望月田吾作",
+    title: "デッド・キャット・バウンス",
+    type: "漫画",
+  },
+  {
+    author: "矢島ともあき",
+    title: "大決戦！鈴木太郎VSニセ鈴木太郎！！？？！？",
+    type: "漫画",
+  },
+  {
+    author: "ゆずお",
+    title: "ぽっぽーまいんど",
+    type: "漫画",
+  },
+  {
+    author: "ゆっちゃん",
+    title: "怪力",
+    type: "漫画",
+  },
+  {
+    author: "ゆぴ太朗",
+    title: "ニウムと俺 あなざ〜でぃめんしょん篇",
+    type: "漫画",
+  },
+  {
+    author: "ゆら",
+    title: "破殻",
+    type: "小説",
+  },
+  {
+    author: "りん",
+    title: "たまご鉄道",
+    type: "イラスト",
+  },
+  {
+    author: "れのさん",
+    title: "ルラとスズカ",
+    type: "イラスト",
+  },
+];
